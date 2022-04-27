c4d728aafa885bd1bd27ed2503015f6349c9659d
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-04-28 01:36:02 +0600

    create api for load data & post data

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { application } = require('express');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 const app = express();
 
 // middleware
-app.use(express());
+app.use(express.json());
 app.use(cors());
+
+// database connectivity
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hpcul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const serviceCollection = client
+      .db('volunteerNetworkDB')
+      .collection('service');
+
+    // get all service
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // POST API
+    app.post('/service', async (req, res) => {
+      const newService = req.body;
+      console.log(newService);
+      console.log('adding new service', newService);
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // api
 app.get('/', (req, res) => {
