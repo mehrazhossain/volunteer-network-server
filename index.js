7c29247d3873555024ac45ebd2af5ae38f6a7502
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-04-28 11:15:41 +0600

    added functionality for update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { application } = require('express');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -41,6 +41,14 @@ async function run() {
       console.log(newService);
       console.log('adding new service', newService);
       const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    // UPDATE a document
+    app.get('/service/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
       res.send(result);
     });
   } finally {
